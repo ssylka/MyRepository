587064a25fcdb18ec5bb5a5f74f590c435ad69b8
Stanislav <75629286+ssylka@users.noreply.github.com>
2021-12-20 16:30:47 +0300

    Мои проекты для изучения c#
    Показывает погоду определённого города с xml-фалов сайта gismeteo

---
+++ C# projects/weather/LibraryFoWeather/Class1.cs
@@ -0,0 +1,35 @@
+﻿            //XDocument doc = new XDocument(new XElement("glossary",
+            //   new XElement("англо-русский",
+            //       new XAttribute("обычный", "с несолькими переводами"),
+            //       new XElement("Company",
+            //            new XElement("компания"),
+            //            new XElement("корпарация")
+            //            )),
+            //            //          new XElement("abduct",
+            //            //new XElement("похищать"),
+            //            //new XElement("оттягивать"),
+            //            //new XElement("отводить")
+            //            //)),
+            //            //           new XElement("abduct",
+            //            //new XElement("похищать"),
+            //            //new XElement("оттягивать"),
+            //            //new XElement("отводить")
+            //            //)),
+            //                      new XElement("abduct",
+            //            new XElement("похищать"),
+            //            new XElement("оттягивать"),
+            //            new XElement("отводить")
+            //            )),
+            //                      new XElement("abduct",
+            //            new XElement("похищать"),
+            //            new XElement("оттягивать"),
+            //            new XElement("отводить")
+            //            )),
+
+            //   new XElement("русско-немецкий",
+            //       new XElement("Company", "Samsung"));
+            //doc.Save("глоссарий.xml");
+
+            //var writer = doc.CreateWriter();
+
+            //var reader = doc.CreateReader();
