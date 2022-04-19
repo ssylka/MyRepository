bc4ff6feca941b23759111ed0f387d8cd568c6f2
Stanislav <75629286+ssylka@users.noreply.github.com>
2022-04-19 15:09:36 +0300

    Add files via upload

---
+++ C# projects/Censorship/Censorship/Form1.Designer.cs
@@ -0,0 +1,204 @@
+﻿
+namespace Censorship
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonPauth = new System.Windows.Forms.Button();
+            this.buttonReturn = new System.Windows.Forms.Button();
+            this.buttonStop = new System.Windows.Forms.Button();
+            this.textBoxIstochnic = new System.Windows.Forms.TextBox();
+            this.textBoxDictionary = new System.Windows.Forms.TextBox();
+            this.textBoxPathOutput = new System.Windows.Forms.TextBox();
+            this.textBoxStatic = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.buttonStart = new System.Windows.Forms.Button();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.SuspendLayout();
+            // 
+            // buttonPauth
+            // 
+            this.buttonPauth.Location = new System.Drawing.Point(58, 128);
+            this.buttonPauth.Name = "buttonPauth";
+            this.buttonPauth.Size = new System.Drawing.Size(75, 23);
+            this.buttonPauth.TabIndex = 0;
+            this.buttonPauth.Text = "пауза";
+            this.buttonPauth.UseVisualStyleBackColor = true;
+            this.buttonPauth.Click += new System.EventHandler(this.buttonPauth_Click);
+            // 
+            // buttonReturn
+            // 
+            this.buttonReturn.Location = new System.Drawing.Point(162, 128);
+            this.buttonReturn.Name = "buttonReturn";
+            this.buttonReturn.Size = new System.Drawing.Size(75, 23);
+            this.buttonReturn.TabIndex = 1;
+            this.buttonReturn.Text = "продолжить";
+            this.buttonReturn.UseVisualStyleBackColor = true;
+            this.buttonReturn.Click += new System.EventHandler(this.buttonReturn_Click);
+            // 
+            // buttonStop
+            // 
+            this.buttonStop.Location = new System.Drawing.Point(264, 128);
+            this.buttonStop.Name = "buttonStop";
+            this.buttonStop.Size = new System.Drawing.Size(75, 23);
+            this.buttonStop.TabIndex = 2;
+            this.buttonStop.Text = "стоп";
+            this.buttonStop.UseVisualStyleBackColor = true;
+            this.buttonStop.Click += new System.EventHandler(this.buttonStop_Click);
+            // 
+            // textBoxIstochnic
+            // 
+            this.textBoxIstochnic.Location = new System.Drawing.Point(138, 12);
+            this.textBoxIstochnic.Name = "textBoxIstochnic";
+            this.textBoxIstochnic.Size = new System.Drawing.Size(258, 20);
+            this.textBoxIstochnic.TabIndex = 3;
+            this.textBoxIstochnic.Text = "C:\\Users\\user\\Desktop\\papka";
+            // 
+            // textBoxDictionary
+            // 
+            this.textBoxDictionary.Location = new System.Drawing.Point(138, 38);
+            this.textBoxDictionary.Name = "textBoxDictionary";
+            this.textBoxDictionary.Size = new System.Drawing.Size(258, 20);
+            this.textBoxDictionary.TabIndex = 4;
+            this.textBoxDictionary.Text = "C:\\Users\\user\\Desktop\\sensor\\mat\\mats.txt";
+            // 
+            // textBoxPathOutput
+            // 
+            this.textBoxPathOutput.Location = new System.Drawing.Point(138, 64);
+            this.textBoxPathOutput.Name = "textBoxPathOutput";
+            this.textBoxPathOutput.Size = new System.Drawing.Size(258, 20);
+            this.textBoxPathOutput.TabIndex = 5;
+            this.textBoxPathOutput.Text = "C:\\Users\\user\\Desktop\\sensor\\new";
+            // 
+            // textBoxStatic
+            // 
+            this.textBoxStatic.Location = new System.Drawing.Point(138, 90);
+            this.textBoxStatic.Name = "textBoxStatic";
+            this.textBoxStatic.Size = new System.Drawing.Size(258, 20);
+            this.textBoxStatic.TabIndex = 6;
+            this.textBoxStatic.Text = "C:\\Users\\user\\Desktop\\sensor\\new\\static";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(80, 13);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Где цензурить";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 44);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(55, 13);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Где маты";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(13, 89);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(90, 13);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Куда статистику";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(13, 67);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(119, 13);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Куда перезаписывать";
+            // 
+            // buttonStart
+            // 
+            this.buttonStart.Location = new System.Drawing.Point(57, 172);
+            this.buttonStart.Name = "buttonStart";
+            this.buttonStart.Size = new System.Drawing.Size(75, 23);
+            this.buttonStart.TabIndex = 11;
+            this.buttonStart.Text = "Старт";
+            this.buttonStart.UseVisualStyleBackColor = true;
+            this.buttonStart.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(147, 172);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(222, 23);
+            this.progressBar1.TabIndex = 12;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(451, 217);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.buttonStart);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBoxStatic);
+            this.Controls.Add(this.textBoxPathOutput);
+            this.Controls.Add(this.textBoxDictionary);
+            this.Controls.Add(this.textBoxIstochnic);
+            this.Controls.Add(this.buttonStop);
+            this.Controls.Add(this.buttonReturn);
+            this.Controls.Add(this.buttonPauth);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonPauth;
+        private System.Windows.Forms.Button buttonReturn;
+        private System.Windows.Forms.Button buttonStop;
+        private System.Windows.Forms.TextBox textBoxIstochnic;
+        private System.Windows.Forms.TextBox textBoxDictionary;
+        private System.Windows.Forms.TextBox textBoxPathOutput;
+        private System.Windows.Forms.TextBox textBoxStatic;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button buttonStart;
+        private System.Windows.Forms.ProgressBar progressBar1;
+    }
+}
+
---
+++ C# projects/Censorship/Censorship/Form1.cs
@@ -0,0 +1,212 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Windows.Forms;
+using System.IO;
+using System.Diagnostics;
+using System.ComponentModel;
+using System.Text.RegularExpressions;
+
+namespace Censorship
+{
+    public partial class Form1 : Form
+    {
+        static List<string> file_static_path_and_weight = new List<string>();
+
+        static List<string> censored_words;
+        static List<string> mati = new List<string>();
+        static string text = "";
+        static string newtext = text;
+        static string[] allfiles;
+        static List<Thread> threads = new List<Thread>();
+        static int j; // счётчик для каждого читаемого файла. ибо передать значение в поток невозможно в thread(((
+        static Semaphore semaphore = new Semaphore(1,60);
+        static List<int> topMats = new List<int>();
+        static List<int> mat10words = new List<int>();
+        static string outputTopMatWords = "";
+        
+        // новый
+
+        List<string> main_directories = new List<string>();
+
+
+
+        public Form1()
+        {
+            InitializeComponent();
+            censored_words = File.ReadAllLines(textBoxDictionary.Text).ToList();
+        }
+        #region Старый код
+        private void PrintStatic()
+        {
+            using (StreamWriter sw = new StreamWriter(textBoxStatic.Text + "\\mats.txt"))
+            {
+
+                foreach (var item in mati)
+                {
+                    sw.WriteLine( mati.Count(s => s.Equals(item)).ToString() + " слова " + item);
+                }
+                sw.WriteLine();
+                sw.WriteLine();
+                foreach (var item in file_static_path_and_weight)
+                {
+                    sw.WriteLine(item);
+                }
+            }
+        }
+
+        private void buttonPauth_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < threads.Count; i++)
+            {   
+                if(threads[i].IsAlive)
+                threads[i].Suspend();
+            }
+        }
+
+        private void buttonReturn_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < threads.Count; i++)
+            {
+                if (threads[i].IsAlive)
+                    threads[i].Resume();
+            }
+        }
+
+        private void buttonStop_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < threads.Count; i++)
+            {
+                if (threads[i].IsAlive)
+                    threads[i].Abort("Завершение по жеанию пользователя");
+            }
+        }
+        #endregion
+        void AddInLisrForStatic(object str)//потому что в потоковом чтении добавитть в статистические лситы значения не получается
+        {
+            text = File.ReadAllText(str.ToString());
+            newtext = text;
+            foreach (var item in censored_words)
+            {
+                string newstr = ""; // нужна для нормальнорго названия нового отцензуренного текста (да и запииси тоже)
+                for (int i = 0; i < str.ToString().Length; i++)
+                {
+                    if (str.ToString()[i] == '\\')
+                    {
+                        newstr = str.ToString().Substring(i);
+                    }
+                }
+                if (text.Contains(item))
+                {
+                    Regex REX = new Regex(item, RegexOptions.IgnoreCase);
+                    MatchCollection RTQ = REX.Matches(text);
+                    Console.ReadLine();
+                    FileInfo fileInfo = new FileInfo(str.ToString());
+                    if(!file_static_path_and_weight.Contains("Мат имеет файл " + str.ToString() + " с весом " + fileInfo.Length.ToString() + " байт"))
+                    file_static_path_and_weight.Add("Мат имеет файл " + str.ToString() + " с весом " + fileInfo.Length.ToString() + " байт");
+                    for (int i = 0; i < RTQ.Count; i++)
+                    {
+                        mati.Add(item);
+                    }
+                    
+                }
+            }
+        }
+
+        void Svoboda_slova_net(object str)
+        {
+            text = File.ReadAllText(str.ToString());
+            newtext = text;
+            foreach (var item in censored_words)
+            {
+                string newstr = ""; // нужна для нормальнорго названия нового отцензуренного текста (да и запииси тоже)
+                for (int i = 0; i < str.ToString().Length; i++)
+                {
+                    if (str.ToString()[i] == '\\')
+                    {
+                        newstr = str.ToString().Substring(i);
+                    }
+                }
+                if (text.Contains(item))
+                {
+                    //if (text.Contains(item))
+                    //{
+                    //    Regex REX = new Regex(item, RegexOptions.IgnoreCase);
+                    //    MatchCollection RTQ = REX.Matches(text);
+                    //    Console.ReadLine();
+                    //    FileInfo fileInfo = new FileInfo(str.ToString());
+                    //    file_static_path_and_weight.Add(str.ToString() + " с весом " + fileInfo.Length.ToString());
+                    //    for (int i = 0; i < RTQ.Count; i++)
+                    //    {
+                    //        mati.Add(item);
+                    //    }
+                    //}
+                    newtext = newtext.Replace(item, "*******");
+
+                    File.WriteAllText(textBoxPathOutput.Text + newstr, newtext);
+                    File.WriteAllText(textBoxPathOutput.Text + newstr.Replace(".txt", "") + " оригинальный.txt", text);
+
+
+
+                }
+
+            }
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            List<string> dirs = Directory.GetDirectories(textBoxIstochnic.Text, "*", SearchOption.AllDirectories).ToList();
+            dirs.Add(textBoxIstochnic.Text); 
+            string[] files_dir;
+            foreach (var item in dirs)
+            {
+                files_dir = Directory.GetFiles(item).Where(s => s.EndsWith(".txt")).ToArray();
+                foreach (var item1 in files_dir)
+                {
+                    Thread thread = new Thread(Svoboda_slova_net);
+
+                    //threads.Add(thread);
+                    //threads.LastOrDefault().Start(item1);
+                    //не работет. почему-то пишет, что ссылка на последний thread не имеет объекта.
+
+                    thread.Start(item1);
+                    AddInLisrForStatic(item1);
+                }
+            }
+            PrintStatic();
+        }
+
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+namespace aaaa
+{
+    class Program
+    {
+    }
+}
+
+
+
+
+
+
