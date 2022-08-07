a79008762ff58b60882e95cb411489d54411cbb1
Stanislav Peresypkin <75629286+ssylka@users.noreply.github.com>
2022-08-07 17:36:00 +0300

    Новый аналог паинт. предназначенный

--- C# projects/Simple_Paint/Simple_Paint/Form1.Designer.cs
+++ C# projects/Simple_Paint/Simple_Paint/Form1.Designer.cs
@@ -30,6 +30,8 @@ namespace Simple_Paint
         private void InitializeComponent()
         {
             this.panel1 = new System.Windows.Forms.Panel();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
             this.labelY = new System.Windows.Forms.Label();
             this.labelX = new System.Windows.Forms.Label();
             this.buttonFill = new System.Windows.Forms.Button();
@@ -41,9 +43,15 @@ namespace Simple_Paint
             this.buttonColor = new System.Windows.Forms.Button();
             this.trackBrush = new System.Windows.Forms.TrackBar();
             this.pictureBoxMain = new System.Windows.Forms.PictureBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.ToolStripMenuItemSaveAndOpen = new System.Windows.Forms.ToolStripMenuItem();
+            this.ToolStripMenuItemOpen = new System.Windows.Forms.ToolStripMenuItem();
+            this.ToolStripMenuItemSave = new System.Windows.Forms.ToolStripMenuItem();
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
             this.panel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.trackBrush)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBoxMain)).BeginInit();
+            this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
             // panel1
@@ -52,6 +60,8 @@ namespace Simple_Paint
             this.panel1.AutoScroll = true;
             this.panel1.AutoSize = true;
             this.panel1.BackgroundImage = global::Simple_Paint.Properties.Resources._49327702_9610_402e_a243_ded9f28d573f;
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.label5);
             this.panel1.Controls.Add(this.labelY);
             this.panel1.Controls.Add(this.labelX);
             this.panel1.Controls.Add(this.buttonFill);
@@ -63,15 +73,34 @@ namespace Simple_Paint
             this.panel1.Controls.Add(this.buttonColor);
             this.panel1.Controls.Add(this.trackBrush);
             this.panel1.Controls.Add(this.pictureBoxMain);
+            this.panel1.Controls.Add(this.menuStrip1);
             this.panel1.Location = new System.Drawing.Point(-2, 2);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(653, 462);
+            this.panel1.Size = new System.Drawing.Size(653, 550);
             this.panel1.TabIndex = 0;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(608, 48);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(14, 13);
+            this.label4.TabIndex = 15;
+            this.label4.Text = "Y";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(608, 30);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(14, 13);
+            this.label5.TabIndex = 14;
+            this.label5.Text = "X";
             // 
             // labelY
             // 
             this.labelY.AutoSize = true;
-            this.labelY.Location = new System.Drawing.Point(598, 21);
+            this.labelY.Location = new System.Drawing.Point(628, 48);
             this.labelY.Name = "labelY";
             this.labelY.Size = new System.Drawing.Size(14, 13);
             this.labelY.TabIndex = 11;
@@ -80,7 +109,7 @@ namespace Simple_Paint
             // labelX
             // 
             this.labelX.AutoSize = true;
-            this.labelX.Location = new System.Drawing.Point(598, 3);
+            this.labelX.Location = new System.Drawing.Point(628, 30);
             this.labelX.Name = "labelX";
             this.labelX.Size = new System.Drawing.Size(14, 13);
             this.labelX.TabIndex = 10;
@@ -90,7 +119,7 @@ namespace Simple_Paint
             // 
             this.buttonFill.BackgroundImage = global::Simple_Paint.Properties.Resources.kisspng_computer_icons_color_grayscale_paint_mixed_color_5af1e47d035205_1956720815258021090136;
             this.buttonFill.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
-            this.buttonFill.Location = new System.Drawing.Point(469, 9);
+            this.buttonFill.Location = new System.Drawing.Point(469, 24);
             this.buttonFill.Name = "buttonFill";
             this.buttonFill.Size = new System.Drawing.Size(44, 37);
             this.buttonFill.TabIndex = 9;
@@ -100,7 +129,7 @@ namespace Simple_Paint
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(519, 21);
+            this.label3.Location = new System.Drawing.Point(519, 36);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(50, 13);
             this.label3.TabIndex = 8;
@@ -109,7 +138,7 @@ namespace Simple_Paint
             // Фигура
             // 
             this.Фигура.AutoSize = true;
-            this.Фигура.Location = new System.Drawing.Point(141, 17);
+            this.Фигура.Location = new System.Drawing.Point(141, 30);
             this.Фигура.Name = "Фигура";
             this.Фигура.Size = new System.Drawing.Size(46, 13);
             this.Фигура.TabIndex = 7;
@@ -125,7 +154,7 @@ namespace Simple_Paint
             "Прямоугольник",
             "Квадрат",
             "Треугольник"});
-            this.BoxFigur.Location = new System.Drawing.Point(14, 13);
+            this.BoxFigur.Location = new System.Drawing.Point(14, 27);
             this.BoxFigur.Name = "BoxFigur";
             this.BoxFigur.Size = new System.Drawing.Size(121, 21);
             this.BoxFigur.TabIndex = 6;
@@ -134,7 +163,7 @@ namespace Simple_Paint
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(233, 17);
+            this.label2.Location = new System.Drawing.Point(233, 31);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(32, 13);
             this.label2.TabIndex = 5;
@@ -143,7 +172,7 @@ namespace Simple_Paint
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(417, 21);
+            this.label1.Location = new System.Drawing.Point(417, 35);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(46, 13);
             this.label1.TabIndex = 4;
@@ -152,7 +181,7 @@ namespace Simple_Paint
             // buttonColor
             // 
             this.buttonColor.BackColor = System.Drawing.Color.Black;
-            this.buttonColor.Location = new System.Drawing.Point(207, 13);
+            this.buttonColor.Location = new System.Drawing.Point(207, 27);
             this.buttonColor.Name = "buttonColor";
             this.buttonColor.Size = new System.Drawing.Size(20, 20);
             this.buttonColor.TabIndex = 3;
@@ -163,7 +192,7 @@ namespace Simple_Paint
             // trackBrush
             // 
             this.trackBrush.BackColor = System.Drawing.SystemColors.ControlLightLight;
-            this.trackBrush.Location = new System.Drawing.Point(287, 3);
+            this.trackBrush.Location = new System.Drawing.Point(287, 21);
             this.trackBrush.Maximum = 100;
             this.trackBrush.Minimum = 5;
             this.trackBrush.Name = "trackBrush";
@@ -176,33 +205,67 @@ namespace Simple_Paint
             // 
             this.pictureBoxMain.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
             this.pictureBoxMain.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.pictureBoxMain.Location = new System.Drawing.Point(1, 57);
+            this.pictureBoxMain.Location = new System.Drawing.Point(1, 67);
             this.pictureBoxMain.MinimumSize = new System.Drawing.Size(653, 405);
             this.pictureBoxMain.Name = "pictureBoxMain";
-            this.pictureBoxMain.Size = new System.Drawing.Size(653, 405);
+            this.pictureBoxMain.Size = new System.Drawing.Size(653, 437);
             this.pictureBoxMain.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
             this.pictureBoxMain.TabIndex = 0;
             this.pictureBoxMain.TabStop = false;
-            this.pictureBoxMain.Click += new System.EventHandler(this.pictureBoxMain_Click);
             this.pictureBoxMain.DragLeave += new System.EventHandler(this.pictureBoxMain_DragLeave);
-            this.pictureBoxMain.Paint += new System.Windows.Forms.PaintEventHandler(this.pictureBoxMain_Paint);
             this.pictureBoxMain.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBoxMain_MouseDown);
             this.pictureBoxMain.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBoxMain_MouseMove);
             this.pictureBoxMain.MouseUp += new System.Windows.Forms.MouseEventHandler(this.pictureBoxMain_MouseUp);
             this.pictureBoxMain.Move += new System.EventHandler(this.pictureBoxMain_MouseMove);
             // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.ToolStripMenuItemSaveAndOpen});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(653, 24);
+            this.menuStrip1.TabIndex = 13;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // ToolStripMenuItemSaveAndOpen
+            // 
+            this.ToolStripMenuItemSaveAndOpen.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.ToolStripMenuItemOpen,
+            this.ToolStripMenuItemSave});
+            this.ToolStripMenuItemSaveAndOpen.Name = "ToolStripMenuItemSaveAndOpen";
+            this.ToolStripMenuItemSaveAndOpen.Size = new System.Drawing.Size(48, 20);
+            this.ToolStripMenuItemSaveAndOpen.Text = "Файл";
+            // 
+            // ToolStripMenuItemOpen
+            // 
+            this.ToolStripMenuItemOpen.Name = "ToolStripMenuItemOpen";
+            this.ToolStripMenuItemOpen.Size = new System.Drawing.Size(180, 22);
+            this.ToolStripMenuItemOpen.Text = "Открыть";
+            this.ToolStripMenuItemOpen.Click += new System.EventHandler(this.ToolStripMenuItemOpen_Click);
+            // 
+            // ToolStripMenuItemSave
+            // 
+            this.ToolStripMenuItemSave.Name = "ToolStripMenuItemSave";
+            this.ToolStripMenuItemSave.Size = new System.Drawing.Size(180, 22);
+            this.ToolStripMenuItemSave.Text = "Сохранить";
+            this.ToolStripMenuItemSave.Click += new System.EventHandler(this.ToolStripMenuItemSave_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(645, 460);
+            this.ClientSize = new System.Drawing.Size(652, 490);
             this.Controls.Add(this.panel1);
+            this.MainMenuStrip = this.menuStrip1;
             this.Name = "Form1";
             this.Text = "FrmMain";
             this.panel1.ResumeLayout(false);
             this.panel1.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.trackBrush)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBoxMain)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -225,6 +288,13 @@ namespace Simple_Paint
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label labelY;
         private System.Windows.Forms.Label labelX;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem ToolStripMenuItemSaveAndOpen;
+        private System.Windows.Forms.ToolStripMenuItem ToolStripMenuItemOpen;
+        private System.Windows.Forms.ToolStripMenuItem ToolStripMenuItemSave;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.ColorDialog colorDialog1;
     }
 }
 
--- C# projects/Simple_Paint/Simple_Paint/Form1.cs
+++ C# projects/Simple_Paint/Simple_Paint/Form1.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -24,22 +25,20 @@ namespace Simple_Paint
         string flagFigure = "Кисть"; // обозначает фигуру, по дефолту - Эллипсис
         ColorDialog colorDialog = new ColorDialog();
         Point one, two;
-
+        Bitmap bmp;
 
         public Form1()
         {
             InitializeComponent();
             this.DesinEnCours = false;
-            g = pictureBoxMain.CreateGraphics();
+            pictureBoxMain.Image = new Bitmap(pictureBoxMain.Width, pictureBoxMain.Height);
+
+            bmp = (Bitmap)pictureBoxMain.Image;
+
+            g = Graphics.FromImage(bmp);
+            pictureBoxMain.Image = bmp;
+
         }
-
-
-
-        private void pictureBoxMain_Paint(object sender, PaintEventArgs e)
-        {
-
-        }
-
         private void pictureBoxMain_MouseDown(object sender, MouseEventArgs e)
         {
             this.DesinEnCours = true;
@@ -62,7 +61,8 @@ namespace Simple_Paint
 
 
                     case "Заливка":
-                        Fill(e.X, e.Y, pen.Color);
+                        //floodFill4(e.X, e.Y, pen.Color, (new Bitmap(pictureBoxMain.Image.Clone() as Bitmap)).GetPixel(e.X, e.Y));
+                        FillByLine(new Point(e.X, e.Y), bmp.GetPixel(e.X, e.Y));
                         break;
 
                     case "Квадрат":
@@ -75,12 +75,11 @@ namespace Simple_Paint
                         g.DrawEllipse(pen, two.X, two.Y, (float)Math.Sqrt( (two.X - one.X)* (two.X - one.X) + (two.Y - one.Y) * (two.Y - one.Y)), (float)Math.Sqrt((two.X - one.X) * (two.X - one.X) + (two.Y - one.Y) * (two.Y - one.Y)));
                         break;
                 }
-                //Des.DesEllipse(g);
-                //System.Windows.Shapes.Ellipse ellipse = new Ellipse();
 
             }
             this.DesinEnCours = false;
             two = e.Location;
+            pictureBoxMain.Image = bmp;
 
         }
 
@@ -96,6 +95,8 @@ namespace Simple_Paint
             {
                 //g.DrawEllipse(pen, e.Location.X, e.Location.Y, 5, 5);
                 g.FillEllipse(new SolidBrush(pen.Color), e.Location.X - pen.Width / 2, e.Location.Y - pen.Width / 2, pen.Width, pen.Width);
+                pictureBoxMain.Image = bmp;
+
             }
             two = e.Location;
             labelX.Text = two.X.ToString();
@@ -107,17 +108,13 @@ namespace Simple_Paint
             pen.Width = trackBrush.Value;
         }
 
-        private void pictureBoxMain_Click(object sender, EventArgs e)
-        {
-        }
 
         private void buttonColor_Click(object sender, EventArgs e)
         {
-            if (colorDialog.ShowDialog() == DialogResult.OK)
-            {
-                pen.Color = colorDialog.Color;
-                buttonColor.BackColor = colorDialog.Color;
-            }
+            if (colorDialog1.ShowDialog() == DialogResult.Cancel)
+                return;
+            pen.Color = colorDialog1.Color;
+            buttonColor.BackColor = pen.Color;
         }
 
         private void BoxFigur_SelectionChangeCommitted(object sender, EventArgs e)
@@ -143,41 +140,30 @@ namespace Simple_Paint
 
             else if (startX < endX && startY > endY)
             {
-                //g.DrawLine(pen, startX, startY, startX + width, startY);
-                //g.DrawLine(pen, startX, startY, startX, startY + width);
-                //g.DrawLine(pen, startX + width, startY, startX + width, startY + width);
-                //g.DrawLine(pen, startX, startY + width, startX + width, startY + width);
-
-
                 g.DrawLine(pen, startX, startY, endX, startY);
                 g.DrawLine(pen, startX, startY, startX, endY);
                 g.DrawLine(pen, endX, startY, endX, endY);
                 g.DrawLine(pen, startX, endY, endX, endY);
-
-
             }
-
             else
             {
                 g.DrawLine(pen, startX, startY, endX, startY);
                 g.DrawLine(pen, startX, startY, startX, endY);
                 g.DrawLine(pen, endX, startY, endX, endY);
                 g.DrawLine(pen, startX, endY, endX, endY);
-
-
             }
         }
 
+        //квадрат
 
         public void DrawSquare(int startX, int startY, int endX, int endY)
         {
             int width = (endX - startX);
-            //квадрат
-
             g.DrawLine(pen, startX, startY, startX + width + pen.Width, startY);
             g.DrawLine(pen, startX, startY, startX, startY + width + pen.Width);
             g.DrawLine(pen, startX + width + pen.Width, startY, startX + width + pen.Width, startY + width + pen.Width);
             g.DrawLine(pen, startX, startY + width + pen.Width, startX + width + pen.Width, startY + width + pen.Width);
+            pictureBoxMain.Image = bmp;
 
         }
 
@@ -190,66 +176,91 @@ namespace Simple_Paint
 
         public void DrawTriangle(int startX, int startY, int endX, int endY)
         {
-
-
             g.DrawLine(pen, startX, startY, endX, endY);
             g.DrawLine(pen, startX, startY, startX, endY);
             g.DrawLine(pen, startX, endY, endX, endY);
+            pictureBoxMain.Image = bmp;
         }
 
         private void pictureBoxMain_MouseMove(object sender, EventArgs e)
         {
             if (flagFigure == "Кисть" && this.DesinEnCours)
             {
-                //    //g.DrawEllipse(pen, e.Location.X, e.Location.Y, 5, 5);
-                //    g.FillEllipse(new SolidBrush(pen.Color),  one.X - pen.Width / 2, one.Y - pen.Width / 2, 5, 5);
                 g.FillEllipse(new SolidBrush(pen.Color), one.X - pen.Width / 2, one.Y - pen.Width / 2, pen.Width, pen.Width);
+                pictureBoxMain.Image = bmp;
 
             }
 
         }
 
-        public void Fill(int x, int y, Color fillColor)
+        //рекурсивный алгоритм заливки на основе серий пикселов (линий)
+        private void FillByLine(Point p, Color oldColor)
         {
-            Bitmap MyBitmap = new Bitmap(x,y); //// битмап - ширина и высота этой битовой карты, а не координата 
-                                                 // к сожалению не работает
-            Color temp = MyBitmap.GetPixel(x-1,y-1);
-
-            int countLeft = x;
-            int countRight = x;
-
-            while (countLeft - 1 > 0 && MyBitmap.GetPixel(countLeft - 1, y) == temp)
+            if (0 <= p.X && p.X < bmp.Width && 0 <= p.Y && p.Y < bmp.Height - 1 && oldColor == bmp.GetPixel(p.X, p.Y) && oldColor != pen.Color)
             {
-                countLeft--;
-            }
-
-            while (countRight + 1 < MyBitmap.Width && MyBitmap.GetPixel(countRight + 1, y) == temp)
-            {
-                countRight++;
-            }
-
-            for (int i = countLeft; i <= countRight; i++)
-            {
-                MyBitmap.SetPixel(i, y, fillColor);
-            }
-
-            for (int i = countLeft; i <= countRight; i++)
-            {
-                if (MyBitmap.GetPixel(i, y - 1) == temp && y - 1 > 0)
+                Point leftBound = new Point(p.X, p.Y);
+                Point rightBound = new Point(p.X, p.Y);
+                Color currentColor = oldColor;
+                while (0 < leftBound.X && currentColor == bmp.GetPixel(--leftBound.X, p.Y)) // X--
                 {
-                    Fill(i, y - 1, fillColor);
+                    currentColor = bmp.GetPixel(leftBound.X, p.Y);
                 }
-
-                if (MyBitmap.GetPixel(i, y + 1) == temp && y + 1 < MyBitmap.Height - 1)
+                currentColor = oldColor;
+                while (rightBound.X < pictureBoxMain.Width - 1 && currentColor == bmp.GetPixel(++rightBound.X, p.Y)) // X++
                 {
-                    Fill(i, y + 1, fillColor);
+                    currentColor = bmp.GetPixel(rightBound.X, p.Y);
                 }
+                g.DrawLine(new Pen(pen.Color,1), leftBound, rightBound);
+                for (int i = leftBound.X; i < rightBound.X + 1; ++i)
+                    FillByLine(new Point(i, p.Y + 1), oldColor);
+                for (int i = leftBound.X; i < rightBound.X + 1; ++i)
+                    if (p.Y > 0)
+                        FillByLine(new Point(i, p.Y - 1), oldColor);
             }
         }
 
+        private void ToolStripMenuItemOpen_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog open_dialog = new OpenFileDialog(); //создание диалогового окна для выбора файла
+            open_dialog.Filter = "Image Files(*.BMP;*.JPG;**.PNG)|*.BMP;*.JPG;**.PNG|All files (*.*)|*.*"; //формат загружаемого файла
+            DialogResult dr = open_dialog.ShowDialog();
+            if (dr == DialogResult.OK)
+            {
+                bmp = new Bitmap(open_dialog.FileName);
+                pictureBoxMain.Image = bmp;
+                g = Graphics.FromImage(bmp);
+
+            }
+
+        }
+
+        private void ToolStripMenuItemSave_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog saveFileDialog1 = new SaveFileDialog();
+            saveFileDialog1.RestoreDirectory = true;
+            saveFileDialog1.Filter = "Image Files(*.JPG)|*.JPG|All files (*.*)|*.*";
+            pictureBoxMain.Image = pictureBoxMain.Image;
+            if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                pictureBoxMain.Image.Save(saveFileDialog1.FileName);
+            }
+
+        }
 
 
+        // Первоначальный алгоритм. Имеет слишком большой стек, поэтому использую другой.
+        void floodFill4(int x, int y, Color newColor, Color oldColor)
+        {
+            Color c = (new Bitmap(pictureBoxMain.Image)).GetPixel(x, y);
+            if (x >= 0 && x < pictureBoxMain.Width && y >= 0 && y < pictureBoxMain.Height && c == oldColor && c != newColor)
+            {
+                g.DrawLine(pen, x, y, x, y);
+                floodFill4(x + 1, y, newColor, oldColor);
+                floodFill4(x - 1, y, newColor, oldColor);
+                floodFill4(x, y + 1, newColor, oldColor);
+                floodFill4(x, y - 1, newColor, oldColor);
+            }
 
-
+        }
     }
 }
